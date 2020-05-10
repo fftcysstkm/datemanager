7c7537246df91e4f4d9d8ac876bb4a996f2d2d76
fftcysstkm <t.satobm3012@gmail.com>
2020-05-11 00:01:36 +0900

    リポジトリクラスのテスト実施、テスト用DBを用意（H2)

--- src/test/java/com/example/demo/BusinessDateDaoImplTest.java
+++ src/test/java/com/example/demo/BusinessDateDaoImplTest.java
@@ -6,7 +6,11 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,14 +18,26 @@ import com.example.demo.repository.BusinessDateDao;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(locations = "/application.properties")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Transactional
 public class BusinessDateDaoImplTest {
+
 	@Autowired
 	BusinessDateDao dao;
 
 	@Test
-	public void カウントメソッドの結果が3件である事() {
-		assertThat(dao.getCount(),is(3));
+	@Sql("/testdata.sql")
+	public void getCountでデフォルト登録件数2件を取得できる事() throws Exception{
+
+		assertThat(dao.getCount(),is(2));
+	}
+
+	@Test(expected = DataAccessException.class)
+	public void selectOneで存在しないIDによる１件取得でエラーが発生する事()throws Exception{
+
+		dao.selectOne(999);
+
 	}
 
 
---
+++ src/test/resorces/testdata.sql
@@ -0,0 +1,7 @@
+--業務日付初期データ
+INSERT INTO datetable
+(date_name, base_date,diff_year, diff_month, diff_day)
+VALUES
+( 'test1','20200108',0,1,0),
+( 'test2','20200203',1,0,0)
+;
