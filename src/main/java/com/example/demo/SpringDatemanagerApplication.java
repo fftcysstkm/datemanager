fb60e47105eab0bf6ac1218eb059280a8dfb5793
fftcysstkm <t.satobm3012@gmail.com>
2020-06-05 13:47:20 +0900

    tomcatデプロイ用のwarファイル作成用設定追記

--- src/main/java/com/example/demo/SpringDatemanagerApplication.java
+++ src/main/java/com/example/demo/SpringDatemanagerApplication.java
@@ -2,9 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringDatemanagerApplication {
+public class SpringDatemanagerApplication extends SpringBootServletInitializer{
+
+	@Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(SpringDatemanagerApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDatemanagerApplication.class, args);
